055886546897107abdd0af591383f7d2c36f416a
Patricio Amaya <patriamayaford@gmail.com>
2026-03-09 12:12:29 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomeContainer from "./components/Home/HomeContainer"
+import DetailContainer from "./components/Detail/DetailContainer"
+import HeaderContainer from "./components/Header/HeaderContainer"
+import FooterContainer from "./components/Footer/FooterContainer"
 
 function App() {
 
   return (
-    <h1>Hola</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<HeaderContainer/>}>
+        <Route element={<FooterContainer/>}>
+        <Route path="/" element={<HomeContainer/>}/>
+        <Route path="/detail/id?" element={<DetailContainer/>}/>
+        </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Detail/DetailContainer.jsx
@@ -0,0 +1,11 @@
+import Detail from "./Detail"
+
+const DetailContainer = () => {
+  return (
+    <div>
+        <Detail/>
+    </div>
+  )
+}
+
+export default DetailContainer
