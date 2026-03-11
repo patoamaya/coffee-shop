6e10a9c61c67efe14048ecd93efbcfecddd353dd
Patricio Amaya <patriamayaford@gmail.com>
2026-03-10 23:15:54 -0300

    designing progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import './Header.css'
+import logo from '../../assets/logo.png'
 import { Outlet } from "react-router-dom"
 
 const Header = () => {
   return (
   <div className='allContainer'>
     <header>
-      <h1>Header</h1>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
     </header>
    <Outlet/>
   </div>
