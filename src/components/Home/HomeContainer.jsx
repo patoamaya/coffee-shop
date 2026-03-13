82d3a9129cebbf1ff2dab899a1088cdaf0d5007a
Patricio Amaya <patriamayaford@gmail.com>
2026-03-12 21:33:08 -0300

    12

--- src/components/Home/HomeContainer.jsx
+++ src/components/Home/HomeContainer.jsx
@@ -1,6 +1,25 @@
 import Home from "./Home"
+import products from '../../products'
+import { useEffect, useState } from "react"
 
 const HomeContainer = () => {
+const [pasteleriaProducts, setPasteleriaProducts] = useState()
+const [cafeteriaProducts, setCafeteriaProducts] = useState()
+
+// console.log(products.cafeteria.map((product)=>setCafeteriaProducts(product)(console.log(product))))
+useEffect(()=>{
+  // console.log(products)
+  setCafeteriaProducts(products.cafeteria)
+  setPasteleriaProducts(products.pasteleria)
+},[])
+// setCafeteriaProducts(products.cafeteria)
+
+console.log(cafeteriaProducts)
+console.log(pasteleriaProducts)
+
+
+// console.log(cafeteriaProducts?.map((product)=>console.log(product)))
+
   return (
     <div>
         <Home/>
